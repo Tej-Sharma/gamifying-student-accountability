a9f5ac4d75a98ef025babe11a6f9f0f5704955e7
Dante Kim <51376601+DantesHub@users.noreply.github.com>
2022-04-30 16:29:09 -0400

    send message to multiple numbers

--- backend/send_sms.js
+++ backend/send_sms.js
@@ -5,10 +5,25 @@ const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')("ACe9c0638e7e63afb1078da19476317d3f", "5e45b21d82b757f761f26152d8856c62");
 
-client.messages
-  .create({
-     body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
-     from: '+19894814693',
-     to: '+12016009948'
-   })
-  .then(message => console.log(message.sid));
+//TODO: add dotenv
+
+const sendMessage = (number, messageBody) => {
+    client.messages
+    .create({
+       body: messageBody,
+       from: '+19894814693',
+       to: number
+     })
+    .then(message => console.log(message.messageBody))
+    .catch(error => console.log(error))
+}
+
+
+const sendAllMessages = (numbers, messageBody) => {
+    numbers.forEach(number => {
+        sendMessage(number, messageBody)
+        console.log("nj")
+    });
+} 
+
+sendAllMessages(["+14845579287", "+12016009948"], "solo leveling");
